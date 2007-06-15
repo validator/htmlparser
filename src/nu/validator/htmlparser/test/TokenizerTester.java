05bf5da4ac4096d8a7048b55d89e35634ea7d54a
hsivonen <none@none>
2007-06-15 11:14:33 +0000

    it might even run
    --HG--
    extra : convert_revision : svn%3A7e3bb964-c432-0410-bb92-eb8c81ae2584/trunk%4022

--- src/nu/validator/htmlparser/test/TokenizerTester.java
+++ src/nu/validator/htmlparser/test/TokenizerTester.java
@@ -1,10 +1,21 @@
 package nu.validator.htmlparser.test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.util.Iterator;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import nu.validator.htmlparser.TokenHandler;
+import nu.validator.htmlparser.Tokenizer;
 
 import com.sdicons.json.model.JSONArray;
 import com.sdicons.json.model.JSONObject;
+import com.sdicons.json.model.JSONString;
 import com.sdicons.json.model.JSONValue;
 import com.sdicons.json.parser.JSONParser;
 
@@ -38,29 +49,53 @@ public class TokenizerTester {
     }
     
     private JSONArray tests;
+    private final JSONArrayTokenHandler tokenHandler;
+    private final Tokenizer tokenizer;
     
     private TokenizerTester(InputStream stream) throws TokenStreamException, RecognitionException {
         JSONParser jsonParser = new JSONParser(stream);
         tests = (JSONArray) ((JSONObject)jsonParser.nextValue()).get("tests");
-        
+        tokenHandler = new JSONArrayTokenHandler();
+        tokenizer = new Tokenizer(tokenHandler);
     }
     
-    private void runTests() {
+    private void runTests() throws SAXException, IOException {
         for (JSONValue val : tests.getValue()) {
             runTest((JSONObject) val);
         }
     }
     
-    private void runTest(JSONObject test) {
-        
+    private void runTest(JSONObject test) throws SAXException, IOException {
+        JSONString inputString = (JSONString) test.get("input");
+        InputSource is = new InputSource(new StringReader(inputString.getValue()));
+        tokenizer.tokenize(is);
+        JSONArray actualTokens = tokenHandler.getArray();
+        JSONArray expectedTokens = (JSONArray) test.get("output");
+        if (jsonDeepEquals(actualTokens, expectedTokens)) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failure");
+            System.out.println(((JSONString)test.get("description")).getValue());
+            System.out.println("Expected tokens:");
+            System.out.println(expectedTokens.render(false));
+            System.out.println("Actual tokens:");
+            System.out.println(actualTokens.render(false));
+        }
     }
     
     /**
      * @param args
+     * @throws RecognitionException 
+     * @throws TokenStreamException 
+     * @throws IOException 
+     * @throws SAXException 
      */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    public static void main(String[] args) throws TokenStreamException, RecognitionException, SAXException, IOException {
+        for (int i = 0; i < args.length; i++) {
+            TokenizerTester tester = new TokenizerTester(new FileInputStream(
+                    args[i]));
+            tester.runTests();
+        }
     }
 
 }
