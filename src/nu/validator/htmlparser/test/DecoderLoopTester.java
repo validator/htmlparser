cd1e6be6721c83e3a784e8e2303aaa25170f8103
hsivonen <none@none>
2007-06-26 10:45:28 +0000

    Fix the decoder loop based on testing
    --HG--
    extra : convert_revision : svn%3A7e3bb964-c432-0410-bb92-eb8c81ae2584/trunk%4055

--- src/nu/validator/htmlparser/test/DecoderLoopTester.java
+++ src/nu/validator/htmlparser/test/DecoderLoopTester.java
@@ -1,105 +1,88 @@
 package nu.validator.htmlparser.test;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
 
 import nu.validator.htmlparser.HtmlInputStreamReader;
 
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+
+import fi.iki.hsivonen.xml.SystemErrErrorHandler;
 
 public class DecoderLoopTester {
     
     private static final int LEAD_OFFSET = 0xD800 - (0x10000 >> 10);
+
+    private static final int NUMBER_OR_ASTRAL_CHARS = 24500;
     
-    private void generateData(File tempFile, boolean write, int padding) throws SAXException, IOException {
-        // eww. this code turned out to be awfully ugly due to repetitive
-        // trial and error. :-( :-( 
-        // not useful to clean it up, though.
-        Charset utf8 = Charset.forName("UTF-8");
-        HtmlInputStreamReader reader = null;
-        char[] readArr = null;
-        OutputStreamWriter writer = null;
-        
-        if (write) {
-            System.out.println("Will write.");
-            writer = new OutputStreamWriter(new FileOutputStream(tempFile), utf8.newEncoder());
-        } else {
-            System.out.println("Will read.");
-            reader = new HtmlInputStreamReader(new FileInputStream(tempFile), null, null, utf8.newDecoder());
-            readArr = new char[1 + padding + 24500 * 2];
-            char[] buf = new char[2048];
-            int currLen = 0;
-            int num = 0;
-            while ((num = reader.read(buf, 0, buf.length)) != -1) {
-                if (num == 0) {
-                    throw new RuntimeException("Decoder read 0 chars.");
-                }
-                currLen += num;
-                System.arraycopy(buf, 0, readArr, currLen, num);
-            }
-            if (currLen != readArr.length) {
-                new RuntimeException("Test failed: file length did not match.");
-            }
-            reader.close();
-        }
-        int readPtr = 0;
-        
-        if (write) {
-            writer.write(0xFEFF);
-        } else {
-            if (readArr[readPtr] != 0xFEFF) {
-                throw new RuntimeException("Test failed: BOM not read.");
-            }
-            readPtr++;
-        }
-        
-        for (int i = 0; i < padding; i++) {
-            if (write) {
-                writer.write('X');                
-            } else {
-                if (readArr[readPtr] != 'X') {
-                    throw new RuntimeException("Test failed: Padding not read.");
-                }                
-            }
-            readPtr++;
-        }
-        
-        char[] astralChar = new char[2];
-        for (int i = 0; i < 24500; i++) {
-            System.out.println("Padding " + padding + " i " + i);
-            int value = 0x10000 + i;
-            astralChar[0] = (char) (LEAD_OFFSET + (value >> 10));
-            astralChar[1] = (char) (0xDC00 + (value & 0x3FF));
-            if (write) {
-                writer.write(astralChar);                
-            } else {
-                if (astralChar[0] != readArr[readPtr]) {
-                    throw new RuntimeException("Test failed: Wrong hi surragate. i="+i);                    
-                }
-                readPtr++;
-                if (astralChar[1] != readArr[readPtr]) {
-                    throw new RuntimeException("Test failed: Wrong lo surragate. i="+i);                    
-                }
-                readPtr++;
-            }
-        }
-        
-        if (write) {
-            writer.flush();
-            writer.close();
-        }
+    private void runTest(int padding) throws SAXException, IOException {
+       Charset utf8 = Charset.forName("UTF-8");
+       char[] charArr = new char[1 + padding + 2 * NUMBER_OR_ASTRAL_CHARS];
+       byte[] byteArr;
+       int i = 0;
+       charArr[i++] = '\uFEFF';
+       for (int j = 0; j < padding; j++) {
+           charArr[i++] = 'x';           
+       }
+       for (int j = 0; j < NUMBER_OR_ASTRAL_CHARS; j++) {
+            int value = 0x10000 + j;
+            charArr[i++] = (char) (LEAD_OFFSET + (value >> 10));
+            charArr[i++] = (char) (0xDC00 + (value & 0x3FF));
+//            charArr[i++] = 'y';
+//            charArr[i++] = 'z';
+
+       }
+       CharBuffer charBuffer = CharBuffer.wrap(charArr);
+       CharsetEncoder enc = utf8.newEncoder();
+       enc.onMalformedInput(CodingErrorAction.REPORT);
+       enc.onUnmappableCharacter(CodingErrorAction.REPORT);
+       ByteBuffer byteBuffer = enc.encode(charBuffer);
+       byteArr = new byte[byteBuffer.limit()];
+       byteBuffer.get(byteArr);
+       
+       ErrorHandler eh = new SystemErrErrorHandler();
+       compare(new HtmlInputStreamReader(new ByteArrayInputStream(byteArr), eh, null), padding, charArr, byteArr);
+       compare(new HtmlInputStreamReader(new ByteArrayInputStream(byteArr), eh, null, utf8.newDecoder()), padding, charArr, byteArr);
+    }
+
+    /**
+     * @param padding
+     * @param charArr
+     * @param byteArr
+     * @throws SAXException
+     * @throws IOException
+     */
+    private void compare(HtmlInputStreamReader reader, int padding, char[] charArr, byte[] byteArr) throws SAXException, IOException {
+           char[] readBuffer = new char[2048];
+           int offset = 0;
+           int num = 0;
+           int readNum = 0;
+           while ((num = reader.read(readBuffer, 0, readBuffer.length)) != -1) {
+               for (int j = 0; j < num; j++) {
+                   System.out.println(offset + j);
+                   if (readBuffer[j] != charArr[offset + j]) {
+                       throw new RuntimeException("Test failed. Char: " + Integer.toHexString(readBuffer[j]) + " j: " + j + " readNum: " + readNum);
+                   }
+               }
+               offset += num;
+               readNum++;
+           }
     }
     
-    void runTests(File tempFile) throws SAXException, IOException {
+    void runTests() throws SAXException, IOException {
         for (int i = 0; i < 4; i++) {
-            generateData(tempFile, true, i);
-            System.out.println("Wrote with padding " + i);
-            generateData(tempFile, false, i);
-            System.out.println("Read with padding " + i);
+            runTest(i);
         }
     }
     
@@ -109,8 +92,7 @@ public class DecoderLoopTester {
      * @throws SAXException 
      */
     public static void main(String[] args) throws IOException, SAXException {
-        File tempFile = File.createTempFile("utf-8-test", ".txt");
-        new DecoderLoopTester().runTests(tempFile);
+        new DecoderLoopTester().runTests();
     }
 
 }
