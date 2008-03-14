72d58f3ccbd7efc5b0482809fc982e31aaefa3c3
hsivonen <none@none>
2008-03-14 10:33:21 +0000

    escape nbsp in serialization (b=012); update legal stuff
    --HG--
    extra : convert_revision : svn%3A7e3bb964-c432-0410-bb92-eb8c81ae2584/trunk%40203

--- src/nu/validator/htmlparser/sax/HtmlSerializer.java
+++ src/nu/validator/htmlparser/sax/HtmlSerializer.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2007 Henri Sivonen
+ * Copyright (c) 2008 Mozilla Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the "Software"), 
@@ -40,17 +41,10 @@ public class HtmlSerializer implements ContentHandler, LexicalHandler {
     private static final String[] VOID_ELEMENTS = { "area", "base", "basefont",
             "bgsound", "br", "col", "embed", "frame", "hr", "img", "input",
             "link", "meta", "param", "spacer", "wbr" };
-    
-    private static final String[] NON_ESCAPING = {"iframe",
-        "noembed",
-        "noframes",
-        "noscript",
-        "plaintext",
-        "script",
-        "style",
-        "xmp"
-    };
-    
+
+    private static final String[] NON_ESCAPING = { "iframe", "noembed",
+            "noframes", "noscript", "plaintext", "script", "style", "xmp" };
+
     private static Writer wrap(OutputStream out) {
         try {
             return new OutputStreamWriter(out, "UTF-8");
@@ -58,13 +52,13 @@ public class HtmlSerializer implements ContentHandler, LexicalHandler {
             throw new RuntimeException(e);
         }
     }
-    
+
     private int ignoreLevel = 0;
-    
+
     private int escapeLevel = 0;
-    
+
     private final Writer writer;
-    
+
     public HtmlSerializer(OutputStream out) {
         this(wrap(out));
     }
@@ -72,33 +66,40 @@ public class HtmlSerializer implements ContentHandler, LexicalHandler {
     public HtmlSerializer(Writer out) {
         this.writer = out;
     }
-    
-    public void characters(char[] ch, int start, int length) throws SAXException {
-            try {
-                if (escapeLevel > 0) {
-                    writer.write(ch, start, length);
-                } else {
-                    for (int i = start; i < start + length; i++) {
-                        char c = ch[i];
-                        switch (c) {
-                            case '<':
-                                writer.write("&lt;");
-                                break;
-                            case '>':
-                                writer.write("&gt;");
-                                break;
-                            case '&':
-                                writer.write("&amp;");
-                                break;
-                            default:
-                                writer.write(c);
-                                break;
-                        }
+
+    public void characters(char[] ch, int start, int length)
+            throws SAXException {
+        try {
+            if (escapeLevel > 0) {
+                writer.write(ch, start, length);
+            } else {
+                for (int i = start; i < start + length; i++) {
+                    char c = ch[i];
+                    switch (c) {
+                        case '"':
+                            writer.write("&quot;");
+                            break;
+                        case '<':
+                            writer.write("&lt;");
+                            break;
+                        case '>':
+                            writer.write("&gt;");
+                            break;
+                        case '&':
+                            writer.write("&amp;");
+                            break;
+                        case '\u00A0':
+                            writer.write("&nbsp;");
+                            break;
+                        default:
+                            writer.write(c);
+                            break;
                     }
                 }
-            } catch (IOException e) {
-                throw new SAXException(e);
-            }           
+            }
+        } catch (IOException e) {
+            throw new SAXException(e);
+        }
     }
 
     public void endDocument() throws SAXException {
@@ -110,7 +111,8 @@ public class HtmlSerializer implements ContentHandler, LexicalHandler {
         }
     }
 
-    public void endElement(String uri, String localName, String qName) throws SAXException {
+    public void endElement(String uri, String localName, String qName)
+            throws SAXException {
         if (escapeLevel > 0) {
             escapeLevel--;
         }
@@ -124,15 +126,17 @@ public class HtmlSerializer implements ContentHandler, LexicalHandler {
                 writer.write('>');
             } catch (IOException e) {
                 throw new SAXException(e);
-            }   
+            }
         }
     }
 
-    public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+    public void ignorableWhitespace(char[] ch, int start, int length)
+            throws SAXException {
         characters(ch, start, length);
     }
 
-    public void processingInstruction(String target, String data) throws SAXException {
+    public void processingInstruction(String target, String data)
+            throws SAXException {
     }
 
     public void setDocumentLocator(Locator locator) {
@@ -146,7 +150,8 @@ public class HtmlSerializer implements ContentHandler, LexicalHandler {
         }
     }
 
-    public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+    public void startElement(String uri, String localName, String qName,
+            Attributes atts) throws SAXException {
         if (escapeLevel > 0) {
             escapeLevel++;
         }
@@ -178,27 +183,31 @@ public class HtmlSerializer implements ContentHandler, LexicalHandler {
                         case '&':
                             writer.write("&amp;");
                             break;
+                        case '\u00A0':
+                            writer.write("&nbsp;");
+                            break;
                         default:
                             writer.write(c);
                             break;
                     }
                 }
-                writer.write('"');                
+                writer.write('"');
             }
             writer.write('>');
             if (Arrays.binarySearch(VOID_ELEMENTS, localName) > -1) {
                 ignoreLevel++;
-                return;                
+                return;
             }
             if ("pre".equals(localName) || "textarea".equals(localName)) {
-                writer.write('\n');                                
+                writer.write('\n');
             }
-            if (escapeLevel == 0 && Arrays.binarySearch(NON_ESCAPING, localName) > -1) {
-                escapeLevel = 1;                
+            if (escapeLevel == 0
+                    && Arrays.binarySearch(NON_ESCAPING, localName) > -1) {
+                escapeLevel = 1;
             }
         } catch (IOException e) {
             throw new SAXException(e);
-        }        
+        }
     }
 
     public void comment(char[] ch, int start, int length) throws SAXException {
@@ -226,13 +235,15 @@ public class HtmlSerializer implements ContentHandler, LexicalHandler {
     public void startCDATA() throws SAXException {
     }
 
-    public void startDTD(String name, String publicId, String systemId) throws SAXException {
+    public void startDTD(String name, String publicId, String systemId)
+            throws SAXException {
     }
 
     public void startEntity(String name) throws SAXException {
     }
 
-    public void startPrefixMapping(String prefix, String uri) throws SAXException {
+    public void startPrefixMapping(String prefix, String uri)
+            throws SAXException {
     }
 
     public void endPrefixMapping(String prefix) throws SAXException {
