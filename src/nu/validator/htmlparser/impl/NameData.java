86e25733f83b0b627c86343150c781a3aebc53ec
hsivonen <none@none>
2008-04-14 10:56:25 +0000

    Land data URIs (b=1) and fix the way NDVL child schema instantiation works

---
+++ src/nu/validator/htmlparser/impl/NameData.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2008 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package nu.validator.htmlparser.impl;
+
+class NameData {
+    public static final String[] ELEMENT_DATA = { "a", "abbr", "abs", "acronym", "address",
+            "altGlyph", "altGlyphDef", "altGlyphItem", "and", "animate",
+            "animateColor", "animateMotion", "animateTransform", "animation",
+            "annotation", "annotation-xml", "applet", "apply", "approx",
+            "arccos", "arccosh", "arccot", "arccoth", "arccsc", "arccsch",
+            "arcsec", "arcsech", "arcsin", "arcsinh", "arctan", "arctanh",
+            "area", "arg", "article", "aside", "audio", "b", "base",
+            "basefont", "bdo", "big", "blockquote", "body", "br", "button",
+            "bvar", "canvas", "caption", "card", "cartesianproduct", "ceiling",
+            "center", "ci", "circle", "cite", "clipPath", "cn", "code",
+            "codomain", "col", "colgroup", "color-profile", "command",
+            "complexes", "compose", "condition", "conjugate", "cos", "cosh",
+            "cot", "coth", "csc", "csch", "csymbol", "curl", "cursor",
+            "datagrid", "datatemplate", "dd", "declare", "definition-src",
+            "defs", "degree", "del", "desc", "details", "determinant", "dfn",
+            "dialog", "diff", "dir", "discard", "div", "divergence", "divide",
+            "dl", "domain", "domainofapplication", "dt", "ellipse", "em",
+            "embed", "emptyset", "eq", "equivalent", "eulergamma",
+            "event-source", "exists", "exp", "exponentiale", "factorial",
+            "factorof", "false", "feBlend", "feColorMatrix",
+            "feComponentTransfer", "feComposite", "feConvolveMatrix",
+            "feDiffuseLighting", "feDisplacementMap", "feDistantLight",
+            "feFlood", "feFuncA", "feFuncB", "feFuncG", "feFuncR",
+            "feGaussianBlur", "feImage", "feMerge", "feMergeNode",
+            "feMorphology", "feOffset", "fePointLight", "feSpecularLighting",
+            "feSpotLight", "feTile", "feTurbulence", "fieldset", "figure",
+            "filter", "floor", "fn", "font", "font-face", "font-face-format",
+            "font-face-name", "font-face-src", "font-face-uri", "footer",
+            "forall", "foreignObject", "form", "frame", "frameset", "g", "gcd",
+            "geq", "glyph", "glyphRef", "grad", "gt", "h1", "h2", "h3", "h4",
+            "h5", "h6", "handler", "head", "header", "hkern", "hr", "html",
+            "i", "ident", "iframe", "image", "imaginary", "imaginaryi", "img",
+            "implies", "in", "infinity", "input", "ins", "int", "integers",
+            "intersect", "interval", "inverse", "isindex", "kbd", "label",
+            "lambda", "laplacian", "lcm", "legend", "leq", "li", "limit",
+            "line", "linearGradient", "link", "list", "listener", "ln", "log",
+            "logbase", "lowlimit", "lt", "maction", "maligngroup",
+            "malignmark", "map", "mark", "marker", "mask", "math", "matrix",
+            "matrixrow", "max", "mean", "median", "menclose", "menu", "merror",
+            "meta", "metadata", "meter", "mfenced", "mfrac", "mglyph", "mi",
+            "min", "minus", "missing-glyph", "mlabeledtr", "mmultiscripts",
+            "mn", "mo", "mode", "moment", "momentabout", "mover", "mpadded",
+            "mpath", "mphantom", "mprescripts", "mroot", "mrow", "ms",
+            "mspace", "msqrt", "mstyle", "msub", "msubsup", "msup", "mtable",
+            "mtd", "mtext", "mtr", "munder", "munderover", "naturalnumbers",
+            "nav", "neq", "nest", "noframes", "none", "noscript", "not",
+            "notanumber", "notin", "notprsubset", "notsubset", "object", "ol",
+            "optgroup", "option", "or", "otherwise", "outerproduct", "output",
+            "p", "param", "partialdiff", "path", "pattern", "pi", "piece",
+            "piecewise", "plus", "polygon", "polyline", "power", "pre",
+            "prefetch", "primes", "product", "progress", "prsubset", "q",
+            "quotient", "radialGradient", "rationals", "real", "reals", "rect",
+            "reln", "rem", "root", "rule", "s", "samp", "scalarproduct",
+            "script", "sdev", "sec", "sech", "section", "select", "selector",
+            "semantics", "sep", "set", "setdiff", "sin", "sinh", "small",
+            "solidColor", "source", "span", "stop", "strike", "strong",
+            "style", "sub", "subset", "sum", "sup", "svg", "switch", "symbol",
+            "table", "tan", "tanh", "tbody", "tbreak", "td", "tendsto", "text",
+            "textPath", "textarea", "tfoot", "th", "thead", "time", "times",
+            "title", "tr", "transpose", "tref", "true", "tspan", "tt", "u",
+            "ul", "union", "uplimit", "use", "var", "variance", "vector",
+            "vectorproduct", "video", "view", "vkern", "xor" };
+}
