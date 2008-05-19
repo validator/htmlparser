4b3051342f5cdd751e72bf0c66c94ff1ec1cd527
hsivonen <none@none>
2008-05-19 10:14:00 +0000

    Add UTF16Buffer
    --HG--
    extra : convert_revision : svn%3A7e3bb964-c432-0410-bb92-eb8c81ae2584/trunk%40232

---
+++ src/nu/validator/htmlparser/impl/UTF16Buffer.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2008 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package nu.validator.htmlparser.impl;
+
+public class UTF16Buffer {
+    private final char[] buffer;
+    private int offset;
+    private int length;
+    
+    /**
+     * @param buffer
+     * @param offset
+     * @param length
+     */
+    public UTF16Buffer(char[] buffer, int offset, int length) {
+        this.buffer = buffer;
+        this.offset = offset;
+        this.length = length;
+    }
+    /**
+     * Returns the offset.
+     * 
+     * @return the offset
+     */
+    public int getOffset() {
+        return offset;
+    }
+    /**
+     * Sets the offset.
+     * 
+     * @param offset the offset to set
+     */
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+    /**
+     * Returns the length.
+     * 
+     * @return the length
+     */
+    public int getLength() {
+        return length;
+    }
+    /**
+     * Sets the length.
+     * 
+     * @param length the length to set
+     */
+    public void setLength(int length) {
+        this.length = length;
+    }
+    /**
+     * Returns the buffer.
+     * 
+     * @return the buffer
+     */
+    public char[] getBuffer() {
+        return buffer;
+    }
+}
