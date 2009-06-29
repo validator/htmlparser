78dbfd7f22a708b98455b89baa1d8c467be97838
hsivonen <none@none>
2009-06-29 09:03:52 +0000

    Make named characters generator use const
    --HG--
    extra : convert_revision : svn%3A7e3bb964-c432-0410-bb92-eb8c81ae2584/trunk%40557

--- translator-src/nu/validator/htmlparser/generator/GenerateNamedCharactersCpp.java
+++ translator-src/nu/validator/htmlparser/generator/GenerateNamedCharactersCpp.java
@@ -55,9 +55,9 @@ import nu.validator.htmlparser.cpptranslate.CppTypes;
 public class GenerateNamedCharactersCpp {
 
     private static final int LEAD_OFFSET = 0xD800 - (0x10000 >> 10);
-    
+
     private static final Pattern LINE_PATTERN = Pattern.compile("^\\s*<tr> <td> <code title=\"\">([^<]*)</code> </td> <td> U\\+(\\S*) </td> </tr>.*$");
-    
+
     private static String toHexString(int c) {
         String hexString = Integer.toHexString(c);
         switch (hexString.length()) {
@@ -74,14 +74,14 @@ public class GenerateNamedCharactersCpp {
         }
     }
 
-    
     /**
      * @param args
-     * @throws IOException 
+     * @throws IOException
      */
     public static void main(String[] args) throws IOException {
         TreeMap<String, String> entities = new TreeMap<String, String>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "utf-8"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                new FileInputStream(args[0]), "utf-8"));
         String line;
         while ((line = reader.readLine()) != null) {
             Matcher m = LINE_PATTERN.matcher(line);
@@ -89,75 +89,88 @@ public class GenerateNamedCharactersCpp {
                 entities.put(m.group(1), m.group(2));
             }
         }
-        
+
         CppTypes cppTypes = new CppTypes(null);
         File targetDirectory = new File(args[1]);
-        
+
         generateH(targetDirectory, cppTypes, entities);
         generateCpp(targetDirectory, cppTypes, entities);
     }
 
-    private static void generateH(File targetDirectory, CppTypes cppTypes, Map<String, String> entities) throws IOException {
-        File hFile = new File(targetDirectory, cppTypes.classPrefix() + "NamedCharacters.h");
-        Writer out = new OutputStreamWriter(new FileOutputStream(hFile), "utf-8");
+    private static void generateH(File targetDirectory, CppTypes cppTypes,
+            Map<String, String> entities) throws IOException {
+        File hFile = new File(targetDirectory, cppTypes.classPrefix()
+                + "NamedCharacters.h");
+        Writer out = new OutputStreamWriter(new FileOutputStream(hFile),
+                "utf-8");
         out.write("#ifndef " + cppTypes.classPrefix() + "NamedCharacters_h__\n");
         out.write("#define " + cppTypes.classPrefix() + "NamedCharacters_h__\n");
         out.write('\n');
-        
+
         String[] includes = cppTypes.namedCharactersIncludes();
         for (int i = 0; i < includes.length; i++) {
             String include = includes[i];
             out.write("#include \"" + include + ".h\"\n");
         }
-        
+
         out.write('\n');
-        
+
         out.write("class " + cppTypes.classPrefix() + "NamedCharacters\n");
         out.write("{\n");
         out.write("  public:\n");
-        out.write("    static " + cppTypes.arrayTemplate() + "<" + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + "," + cppTypes.intType() + ">," + cppTypes.intType() + "> NAMES;\n");
-        out.write("    static " + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + "," + cppTypes.intType() + ">* VALUES;\n");
+        out.write("    static " + cppTypes.arrayTemplate() + "<"
+                + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + ","
+                + cppTypes.intType() + ">," + cppTypes.intType() + "> NAMES;\n");
+        out.write("    static " + cppTypes.arrayTemplate() + "<"
+                + cppTypes.charType() + "," + cppTypes.intType()
+                + ">* VALUES;\n");
         out.write("    static " + cppTypes.charType() + "** WINDOWS_1252;\n");
         out.write("    static void initializeStatics();\n");
         out.write("    static void releaseStatics();\n");
-        out.write("  private:\n");
-        out.write("#ifdef " + cppTypes.classPrefix() + "NamedCharacters_cpp__\n");
-        out.write("    static " + cppTypes.charType() + " WINDOWS_1252_DATA[];\n");
-
-        for (int i = 0; i < entities.size(); i++) {
-            out.write("    static " + cppTypes.charType() + " NAME_" + i + "[];\n");
-            out.write("    static " + cppTypes.charType() + " VALUE_" + i + "[];\n");
-        }
-
-        out.write("#endif\n");
         out.write("};\n");
-        
-        out.write("\n#endif // " + cppTypes.classPrefix() + "NamedCharacters_h__\n");
+
+        out.write("\n#endif // " + cppTypes.classPrefix()
+                + "NamedCharacters_h__\n");
         out.flush();
         out.close();
     }
 
-    private static void generateCpp(File targetDirectory, CppTypes cppTypes, Map<String, String> entities) throws IOException {
-        File hFile = new File(targetDirectory, cppTypes.classPrefix() + "NamedCharacters.cpp");
-        Writer out = new OutputStreamWriter(new FileOutputStream(hFile), "utf-8");
+    private static void generateCpp(File targetDirectory, CppTypes cppTypes,
+            Map<String, String> entities) throws IOException {
+        File hFile = new File(targetDirectory, cppTypes.classPrefix()
+                + "NamedCharacters.cpp");
+        Writer out = new OutputStreamWriter(new FileOutputStream(hFile),
+                "utf-8");
 
-        out.write("#define " + cppTypes.classPrefix() + "NamedCharacters_cpp__\n");
-        
+        out.write("#define " + cppTypes.classPrefix()
+                + "NamedCharacters_cpp__\n");
+
         String[] includes = cppTypes.namedCharactersIncludes();
         for (int i = 0; i < includes.length; i++) {
             String include = includes[i];
             out.write("#include \"" + include + ".h\"\n");
         }
-        
+
         out.write('\n');
-        out.write("#include \"" + cppTypes.classPrefix() + "NamedCharacters.h\"\n");
+        out.write("#include \"" + cppTypes.classPrefix()
+                + "NamedCharacters.h\"\n");
         out.write("\n");
 
-        out.write("" + cppTypes.arrayTemplate() + "<" + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + "," + cppTypes.intType() + ">," + cppTypes.intType() + "> " + cppTypes.classPrefix() + "NamedCharacters::NAMES = " + cppTypes.arrayTemplate() + "<" + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + "," + cppTypes.intType() + ">," + cppTypes.intType() + ">();\n");
-        out.write("" + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + "," + cppTypes.intType() + ">* " + cppTypes.classPrefix() + "NamedCharacters::VALUES = " + cppTypes.nullLiteral() + ";\n");
-        out.write("" + cppTypes.charType() + "** " + cppTypes.classPrefix() + "NamedCharacters::WINDOWS_1252 = " + cppTypes.nullLiteral() + ";\n");
-        
-        out.write("" + cppTypes.charType() + " " + cppTypes.classPrefix() + "NamedCharacters::WINDOWS_1252_DATA[] = {\n");
+        out.write("" + cppTypes.arrayTemplate() + "<"
+                + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + ","
+                + cppTypes.intType() + ">," + cppTypes.intType() + "> "
+                + cppTypes.classPrefix() + "NamedCharacters::NAMES = "
+                + cppTypes.arrayTemplate() + "<" + cppTypes.arrayTemplate()
+                + "<" + cppTypes.charType() + "," + cppTypes.intType() + ">,"
+                + cppTypes.intType() + ">();\n");
+        out.write("" + cppTypes.arrayTemplate() + "<" + cppTypes.charType()
+                + "," + cppTypes.intType() + ">* " + cppTypes.classPrefix()
+                + "NamedCharacters::VALUES = " + cppTypes.nullLiteral() + ";\n");
+        out.write("" + cppTypes.charType() + "** " + cppTypes.classPrefix()
+                + "NamedCharacters::WINDOWS_1252 = " + cppTypes.nullLiteral()
+                + ";\n");
+
+        out.write("static " + cppTypes.charType() + " const WINDOWS_1252_DATA[] = {\n");
         out.write("  0x20AC,\n");
         out.write("  0xFFFD,\n");
         out.write("  0x201A,\n");
@@ -191,15 +204,16 @@ public class GenerateNamedCharactersCpp {
         out.write("  0x017E,\n");
         out.write("  0x0178\n");
         out.write("};\n");
-        
+
         int k = 0;
         for (Map.Entry<String, String> entity : entities.entrySet()) {
             String name = entity.getKey();
             int value = Integer.parseInt(entity.getValue(), 16);
 
-            out.write(cppTypes.charType() + " " + cppTypes.classPrefix() + "NamedCharacters::NAME_" + k + "[] = {\n");
+            out.write("static " + cppTypes.charType() + " const NAME_" + k
+                    + "[] = {\n");
             out.write("  ");
-            
+
             for (int j = 0; j < name.length(); j++) {
                 char c = name.charAt(j);
                 if (j != 0) {
@@ -209,55 +223,72 @@ public class GenerateNamedCharactersCpp {
                 out.write(c);
                 out.write('\'');
             }
-            
+
             out.write("\n};\n");
-            
-            out.write(cppTypes.charType() + " " + cppTypes.classPrefix() + "NamedCharacters::VALUE_" + k + "[] = {\n");
+
+            out.write("static " + cppTypes.charType() + " const VALUE_" + k
+                    + "[] = {\n");
             out.write("  ");
-            
+
             if (value <= 0xFFFF) {
-                out.write(toHexString(value));                                
+                out.write(toHexString(value));
             } else {
                 int hi = (LEAD_OFFSET + (value >> 10));
                 int lo = (0xDC00 + (value & 0x3FF));
-                out.write(toHexString(hi));                                
-                out.write(", ");                
-                out.write(toHexString(lo));                             
+                out.write(toHexString(hi));
+                out.write(", ");
+                out.write(toHexString(lo));
             }
-            
+
             out.write("\n};\n");
 
             k++;
         }
 
-        
         out.write("void\n");
-        out.write(cppTypes.classPrefix() + "NamedCharacters::initializeStatics()\n");
+        out.write(cppTypes.classPrefix()
+                + "NamedCharacters::initializeStatics()\n");
         out.write("{\n");
-        out.write("  NAMES = " + cppTypes.arrayTemplate() + "<" + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + "," + cppTypes.intType() + ">," + cppTypes.intType() + ">(" + entities.size() + ");\n");
-        for (int i = 0; i < entities.size(); i++) {        
-            out.write("  NAMES[" + i + "] = " + cppTypes.staticArrayMacro() + "(" + cppTypes.charType() + ", " + cppTypes.intType() + ", NAME_" + i + ");\n");
-        }        
-        out.write("  VALUES = new " + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + "," + cppTypes.intType() + ">[" + entities.size() + "];\n");
-        for (int i = 0; i < entities.size(); i++) {        
-            out.write("  VALUES[" + i + "] = " + cppTypes.staticArrayMacro() + "(" + cppTypes.charType() + ", " + cppTypes.intType() + ", VALUE_" + i + ");\n");
-        } 
+        out.write("  NAMES = " + cppTypes.arrayTemplate() + "<"
+                + cppTypes.arrayTemplate() + "<" + cppTypes.charType() + ","
+                + cppTypes.intType() + ">," + cppTypes.intType() + ">("
+                + entities.size() + ");\n");
+        int i = 0;
+        for (Map.Entry<String, String> entity : entities.entrySet()) {
+            out.write("  NAMES[" + i + "] = " + cppTypes.arrayTemplate() + "<"
+                    + cppTypes.charType() + "," + cppTypes.intType() + ">(("
+                    + cppTypes.charType() + "*)NAME_" + i + ", "
+                    + entity.getKey().length() + ");\n");
+            i++;
+        }
+        out.write("  VALUES = new " + cppTypes.arrayTemplate() + "<"
+                + cppTypes.charType() + "," + cppTypes.intType() + ">["
+                + entities.size() + "];\n");
+        i = 0;
+        for (Map.Entry<String, String> entity : entities.entrySet()) {
+            int value = Integer.parseInt(entity.getValue(), 16);
+            out.write("  VALUES[" + i + "] = " + cppTypes.arrayTemplate() + "<"
+                    + cppTypes.charType() + "," + cppTypes.intType() + ">(("
+                    + cppTypes.charType() + "*)VALUE_" + i + ", "
+                    + ((value <= 0xFFFF) ? "1" : "2") + ");\n");
+            i++;
+        }
         out.write("\n");
         out.write("  WINDOWS_1252 = new " + cppTypes.charType() + "*[32];\n");
         out.write("  for (" + cppTypes.intType() + " i = 0; i < 32; ++i) {\n");
-        out.write("    WINDOWS_1252[i] = &(WINDOWS_1252_DATA[i]);\n");
+        out.write("    WINDOWS_1252[i] = (" + cppTypes.charType() + "*)&(WINDOWS_1252_DATA[i]);\n");
         out.write("  }\n");
         out.write("}\n");
         out.write("\n");
         out.write("void\n");
-        out.write(cppTypes.classPrefix() + "NamedCharacters::releaseStatics()\n");
+        out.write(cppTypes.classPrefix()
+                + "NamedCharacters::releaseStatics()\n");
         out.write("{\n");
         out.write("  NAMES.release();\n");
         out.write("  delete[] VALUES;\n");
-        out.write("  delete[] WINDOWS_1252;\n");
         out.write("}\n");
         out.write("\n");
         out.flush();
         out.close();
-    }    
+    }
 }
