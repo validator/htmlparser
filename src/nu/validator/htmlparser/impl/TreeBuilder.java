0c397229b2cba1ed42d7023b2e259f518df2da3c
Henri Sivonen <hsivonen@iki.fi>
2011-01-07 19:20:36 +0200

    Check errorHandler for null before reporting unclosed tags on EOF in TEXT.

--- src/nu/validator/htmlparser/impl/TreeBuilder.java
+++ src/nu/validator/htmlparser/impl/TreeBuilder.java
@@ -1452,8 +1452,10 @@ public abstract class TreeBuilder<T> implements TokenHandler,
                     // ]NOCPP]
                     break eofloop;
                 case TEXT:
-                    err("End of file seen when expecting text or an end tag.");
-                    errListUnclosedStartTags(0);
+                    if (errorHandler != null) {
+                        errNoCheck("End of file seen when expecting text or an end tag.");
+                        errListUnclosedStartTags(0);
+                    }
                     // XXX mark script as already executed
                     if (originalMode == AFTER_HEAD) {
                         popOnEof();
