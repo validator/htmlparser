2e125f35b654e22fefad6283cdb6b8f3df0bde78
Michael[tm] Smith <mike@w3.org>
2016-07-02 21:02:43 +0900

    Experimentally special-case “Changing encoding” err
    Many real-world HTML documents on the Web contain `meta` elements deep
    inside the documents which trigger the parser to emit a “Changing encoding
    at this point would need non-streamable behavior.” error. So when the
    parser is used in the context of the HTML checker, that error gets output
    to the log so often, with an accompanying stack trace, that it basically
    just generates a unwelcome noise in the logs.
    
    This change creates a special exception for this case, so that we can catch
    it in the HTML checker code (and prevent the stack trace from getting
    logged every single time).

---
+++ src/nu/validator/htmlparser/io/ChangingEncodingException.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package nu.validator.htmlparser.io;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class ChangingEncodingException extends SAXParseException {
+    public ChangingEncodingException(ErrorHandler errorHandler, Locator locator)
+            throws SAXException {
+        super("Changing encoding at this point would need non-streamable behavior.",
+                locator);
+        if (errorHandler != null) {
+            errorHandler.fatalError(this);
+        }
+    }
+}
