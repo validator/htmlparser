validator/htmlparser

commit 2f61c94cebcee834f7fbc30f6efde281b41ee9ba

Ensure every Locator is also a Locator2 (#10)
* Replace org.xml.sax.helpers.LocatorImpl with nu.validator.htmlparser.impl.LocatorImpl

* instanceof before cast to Locator2

src/nu/validator/saxtree/DocumentFragment.java
@@ -23,7 +23,7 @@
 
 package nu.validator.saxtree;
 
-import org.xml.sax.helpers.LocatorImpl;
+import nu.validator.htmlparser.impl.LocatorImpl;
 
 /**
  * A document fragment.

src/nu/validator/htmlparser/impl/LocatorImpl.java
@@ -24,22 +24,38 @@
 package nu.validator.htmlparser.impl;
 
 import org.xml.sax.Locator;
+import org.xml.sax.ext.Locator2;
 
-public class LocatorImpl implements Locator {
+public class LocatorImpl implements Locator, Locator2 {
 
     private final String systemId;
 
     private final String publicId;
 
+    private final String encoding;
+
     private final int column;
 
     private final int line;
+
+    /**
+     * Create a new Locator with default values
+     */
+    public LocatorImpl() {
+        this.systemId = this.publicId = this.encoding = null;
+        this.column = this.line = 0;
+    }
 
     public LocatorImpl(Locator locator) {
         this.systemId = locator.getSystemId();
         this.publicId = locator.getPublicId();
         this.column = locator.getColumnNumber();
         this.line = locator.getLineNumber();
+        if (locator instanceof Locator2) {
+            this.encoding = ((Locator2)locator).getEncoding();
+        } else {
+            this.encoding = null;
+        }
     }
 
     public final int getColumnNumber() {
@@ -56,5 +72,13 @@ public class LocatorImpl implements Locator {
 
     public final String getSystemId() {
         return systemId;
+    }
+
+    public final String getXMLVersion() {
+        return "1.0";
+    }
+
+    public final String getEncoding() {
+        return encoding;
     }
 }

src/nu/validator/htmlparser/impl/Tokenizer.java
@@ -37,6 +37,7 @@ package nu.validator.htmlparser.impl;
 
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.Locator;
+import org.xml.sax.ext.Locator2;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
@@ -66,7 +67,7 @@ import nu.validator.htmlparser.common.XmlViolationPolicy;
  * @version $Id$
  * @author hsivonen
  */
-public class Tokenizer implements Locator {
+public class Tokenizer implements Locator, Locator2 {
 
     private static final int DATA_AND_RCDATA_MASK = ~1;
 
@@ -862,6 +863,24 @@ public class Tokenizer implements Locator {
      */
     public String getSystemId() {
         return systemId;
+    }
+
+    /**
+     * @see org.xml.sax.ext.Locator2#getXMLVersion()
+     */
+    public String getXMLVersion() {
+        return "1.0";
+    }
+
+    /**
+     * @see org.xml.sax.ext.Locator2#getXMLVersion()
+     */
+    public String getEncoding() {
+        try {
+            return encodingDeclarationHandler == null ? null : encodingDeclarationHandler.getCharacterEncoding();
+        } catch (SAXException e) {
+            return null;
+        }
     }
 
     // end Locator impl

src/nu/validator/htmlparser/io/Driver.java
@@ -579,7 +579,7 @@ public class Driver implements EncodingDeclarationHandler {
     }
 
     public String getCharacterEncoding() throws SAXException {
-        return characterEncoding.getCanonName();
+        return characterEncoding == null ? null : characterEncoding.getCanonName();
     }
 
     public Locator getDocumentLocator() {

src/nu/validator/htmlparser/io/HtmlInputStreamReader.java
@@ -42,6 +42,7 @@ import nu.validator.htmlparser.impl.Tokenizer;
 
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.Locator;
+import org.xml.sax.ext.Locator2;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
@@ -58,7 +59,7 @@ import org.xml.sax.SAXParseException;
  * @author hsivonen
  */
 public final class HtmlInputStreamReader extends Reader implements
-        ByteReadable, Locator {
+        ByteReadable, Locator, Locator2 {
 
     private static final int SNIFFING_LIMIT = 1024;
 
@@ -448,6 +449,20 @@ public final class HtmlInputStreamReader extends Reader implements
     public String getSystemId() {
         if (tokenizer != null) {
             return tokenizer.getSystemId();
+        }
+        return null;
+    }
+
+    public String getXMLVersion() {
+        if (tokenizer != null) {
+            return tokenizer.getXMLVersion();
+        }
+        return null;
+    }
+
+    public String getEncoding() {
+        if (tokenizer != null) {
+            return tokenizer.getEncoding();
         }
         return null;
     }

src/nu/validator/htmlparser/io/MetaSniffer.java
@@ -30,10 +30,11 @@ import nu.validator.htmlparser.impl.MetaScanner;
 
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.Locator;
+import org.xml.sax.ext.Locator2;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
-public class MetaSniffer extends MetaScanner implements Locator {
+public class MetaSniffer extends MetaScanner implements Locator, Locator2 {
     
     private Encoding characterEncoding = null;
 
@@ -139,6 +140,20 @@ public class MetaSniffer extends MetaScanner implements Locator {
     public String getSystemId() {
         if (locator != null) {
             return locator.getSystemId();
+        }
+        return null;
+    }
+
+    public String getXMLVersion() {
+        if (locator != null) {
+            return ((Locator2)locator).getXMLVersion();
+        }
+        return null;
+    }
+
+    public String getEncoding() {
+        if (locator != null) {
+            return ((Locator2)locator).getEncoding();
         }
         return null;
     }

src/nu/validator/saxtree/LocatorImpl.java
@@ -24,13 +24,14 @@
 package nu.validator.saxtree;
 
 import org.xml.sax.Locator;
+import org.xml.sax.ext.Locator2;
 
 /**
  * A locator implementation.
  * @version $Id$
  * @author hsivonen
  */
-public final class LocatorImpl implements Locator {
+public final class LocatorImpl implements Locator, Locator2 {
 
     /**
      * The system id.
@@ -41,6 +42,11 @@ public final class LocatorImpl implements Locator {
      * The public id.
      */
     private final String publicId;
+    
+    /**
+     * The encoding.
+     */
+    private final String encoding;
     
     /**
      * The column.
@@ -62,11 +68,17 @@ public final class LocatorImpl implements Locator {
             this.publicId = null;
             this.column = -1;
             this.line = -1;
+            this.encoding = null;
         } else {
             this.systemId = locator.getSystemId();
             this.publicId = locator.getPublicId();
             this.column = locator.getColumnNumber();
             this.line = locator.getLineNumber();
+            if (locator instanceof Locator2) {
+                this.encoding = ((Locator2)locator).getEncoding();
+            } else {
+                this.encoding = null;
+            }
         }
     }
     
@@ -101,4 +113,21 @@ public final class LocatorImpl implements Locator {
     public String getSystemId() {
         return systemId;
     }
+
+    /**
+     * 
+     * @see org.xml.sax.ext.Locator2#getXMLVersion()
+     */
+    public String getXMLVersion() {
+        return "1.0";
+    }
+
+    /**
+     * 
+     * @see org.xml.sax.ext.Locator2#getEncoding()
+     */
+    public String getEncoding() {
+        return encoding;
+    }
+
 }

src/nu/validator/saxtree/Node.java
@@ -27,6 +27,7 @@ import java.util.List;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.Locator;
+import org.xml.sax.ext.Locator2;
 import org.xml.sax.SAXException;
 
 /**
@@ -34,7 +35,7 @@ import org.xml.sax.SAXException;
  * @version $Id$
  * @author hsivonen
  */
-public abstract class Node implements Locator {
+public abstract class Node implements Locator, Locator2 {
 
     /**
      * The system id.
@@ -45,6 +46,11 @@ public abstract class Node implements Locator {
      * The public id.
      */
     private final String publicId;
+
+    /**
+     * The encoding.
+     */
+    private final String encoding;
     
     /**
      * The column.
@@ -75,6 +81,7 @@ public abstract class Node implements Locator {
         if (locator == null) {
             this.systemId = null;
             this.publicId = null;
+            this.encoding = null;
             this.column = -1;
             this.line = -1;
         } else {
@@ -82,6 +89,11 @@ public abstract class Node implements Locator {
             this.publicId = locator.getPublicId();
             this.column = locator.getColumnNumber();
             this.line = locator.getLineNumber();
+            if (locator instanceof Locator2) {
+                this.encoding = ((Locator2)locator).getEncoding();
+            } else {
+                this.encoding = null;
+            }
         }
     }
     
@@ -115,6 +127,20 @@ public abstract class Node implements Locator {
      */
     public String getSystemId() {
         return systemId;
+    }
+
+    /**
+     * @see org.xml.sax.ext.Locator2#getXMLVersion()
+     */
+    public String getXMLVersion() {
+        return "1.0";
+    }
+
+    /**
+     * @see org.xml.sax.ext.Locator2#getEncoding
+     */
+    public String getEncoding() {
+        return encoding;
     }
 
     /**

commit 777c1f248777f5e25716b86c24c89bd715ca5a5d

Break cyclic dependency
2f61c94 introduced a cyclic dependency between saxtree & htmlparser.

src/nu/validator/saxtree/DocumentFragment.java
@@ -23,7 +23,7 @@
 
 package nu.validator.saxtree;
 
-import nu.validator.htmlparser.impl.LocatorImpl;
+import org.xml.sax.ext.Locator2Impl;
 
 /**
  * A document fragment.
@@ -37,7 +37,7 @@ public final class DocumentFragment extends ParentNode {
      * The constructor.
      */
     public DocumentFragment() {
-        super(new LocatorImpl());
+        super(new Locator2Impl());
     }
 
     /**