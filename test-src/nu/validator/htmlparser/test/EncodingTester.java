e8e4a25fe1c533c7c1f43b2b43926d90672ebe2d
Michael[tm] Smith <mike@w3.org>
2020-08-21 13:49:57 +0900

    Make EncodingTester usable in testing parsed state
    This change updates EncodingTester to make it test the result for cases
    when the expected character encoding is not limited to what can be
    determined by checking only the first 1024 bytes of the input stream.
    
    Otherwise, without this change, EncodingTester is limited to only being
    useful for testing the output of the meta prescan.

--- test-src/nu/validator/htmlparser/test/EncodingTester.java
+++ test-src/nu/validator/htmlparser/test/EncodingTester.java
@@ -38,6 +38,8 @@ public class EncodingTester {
 
     private static int exitStatus = 0;
 
+    protected static int SNIFFING_LIMIT = 16384;
+
     private final InputStream aggregateStream;
 
     private final StringBuilder builder = new StringBuilder();
@@ -61,7 +63,7 @@ public class EncodingTester {
         }
         UntilHashInputStream stream = new UntilHashInputStream(aggregateStream);
         HtmlInputStreamReader reader = new HtmlInputStreamReader(stream, null,
-                null, null, Heuristics.NONE);
+                null, null, Heuristics.NONE, SNIFFING_LIMIT);
         Charset charset = reader.getCharset();
         stream.close();
         if (skipLabel()) {
